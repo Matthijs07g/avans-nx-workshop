183b7972454ce22a38326c297187ad9024717396
Matthijs <matthijs07g@gmail.com>
2023-11-20 11:24:03 +0100

    small changes

--- libs/share-a-meal/dotenv.ts
+++ libs/share-a-meal/dotenv.ts
@@ -5,5 +5,5 @@ const env = parse(process.env as unknown as string) as DotenvParseOutput;
 
 export const environment = {
   production: true,
-  apiEndpoint: env.API_ENDPOINT || 'avans-nx-matthijs.azurewebsites.net',
+  apiEndpoint: env['API_ENDPOINT'] || 'avans-nx-matthijs.azurewebsites.net',
 };
