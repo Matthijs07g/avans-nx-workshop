0c01a2f5f610ff00ee72e417cd533d742381e7e9
Matthijs <matthijs07g@gmail.com>
2023-12-10 19:47:14 +0100

    small changes on about-page

--- apps/data-api/src/app/app.module.ts
+++ apps/data-api/src/app/app.module.ts
@@ -33,17 +33,3 @@ import { Neo4jModule, Neo4jScheme } from "nest-neo4j/dist";
     providers: []
 })
 export class AppModule {}
-
-
-// Neo4jModule.forRootAsync({
-//     imports: [
-//     ],
-//     inject: [ ConfigService ],
-//     useFactory: (configService: ConfigService): Neo4jConfig => ({
-//       scheme: configService.get('NEO4J_SCHEME') || 'neo4j',
-//       host: configService.get('NEO4J_HOST') || 'localhost',
-//       port: configService.get('NEO4J_PORT') || 7687,
-//       username: configService.get('NEO4J_USERNAME') || 'neo4j',
-//       password: configService.get('NEO4J_PASSWORD') || '',
-//       database: configService.get('NEO4J_DATABASE') || 'neo4j',
-//     })}),
