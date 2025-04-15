d794e33132d8810f7704553a366cf5711d208865
Matthijs07g <matthijs07g@gmail.com>
2025-04-15 16:11:29 +0200

    update MongoDB connection string to use Atlas for F1Blogger

--- libs/shared/util-env/src/lib/environment.ts
+++ libs/shared/util-env/src/lib/environment.ts
@@ -3,7 +3,7 @@ import { IEnvironment } from "./environment.interface";
 export const environment: IEnvironment = {
   production: false,
   apiUrl: 'http://localhost:3000/api/',
-  MONGO_DB_CONNECTION_STRING: 'mongodb://127.0.0.1:27017/F1Blogger',
+  MONGO_DB_CONNECTION_STRING: 'mongodb+srv://Guest:82cvM6Ykf7Augf2@f1blogger.itow09w.mongodb.net/?retryWrites=true&w=majority&appName=F1Blogger',
   NEO4J_SCHEME: 'neo4j',
   NEO4J_HOST: 'localhost',
   NEO4J_PORT: '7687',
