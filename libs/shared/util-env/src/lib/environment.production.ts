ce339d734abaa377e783ca7298207ef701e165f8
Matthijs07g <matthijs07g@gmail.com>
2025-04-15 22:41:56 +0200

    refactor: update apiUrl in environment.production.ts for correct endpoint

--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -2,6 +2,6 @@ import { IEnvironment } from "./environment.interface";
 
 export const environment: IEnvironment = {
     production: true,
-    apiUrl: 'https://f1blogger.azurewebsites.net',
+    apiUrl: 'https://f1blogger.azurewebsites.net/api/',
     MONGO_DB_CONNECTION_STRING: 'mongodb+srv://Guest:82cvM6Ykf7Augf2@f1blogger.itow09w.mongodb.net/?retryWrites=true&w=majority&appName=F1Blogger',
 }
