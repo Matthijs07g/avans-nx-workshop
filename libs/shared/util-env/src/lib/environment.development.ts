f4e7dcbc1392f643b901cbbf52c0e76ba325b171
Matthijs07g <matthijs07g@gmail.com>
2025-04-15 20:24:23 +0200

    refactor: remove Neo4j configuration from environment files

--- libs/shared/util-env/src/lib/environment.development.ts
+++ libs/shared/util-env/src/lib/environment.development.ts
@@ -3,11 +3,5 @@ import { IEnvironment } from "./environment.interface";
 export const environment: IEnvironment = {
     production: false,
     apiUrl: 'http://localhost:3000/api/',
-    MONGO_DB_CONNECTION_STRING: 'mongodb://127.0.0.1:27017/F1Blogger',
-    NEO4J_SCHEME: 'neo4j',
-    NEO4J_HOST: 'localhost',
-    NEO4J_PORT: '7687',
-    NEO4J_USERNAME: 'neo4j',
-    NEO4J_PASSWORD: 'guest',
-    NEO4J_DATABASE: 'F1BloggerUsers',
+    MONGO_DB_CONNECTION_STRING: 'mongodb://127.0.0.1:27017/F1Blogger'
 }
--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -2,12 +2,6 @@ import { IEnvironment } from "./environment.interface";
 
 export const environment: IEnvironment = {
     production: true,
-    apiUrl: 'https://f1blogger.azurewebsites.net/api/',
+    apiUrl: 'https://f1blogger.azurewebsites.net',
     MONGO_DB_CONNECTION_STRING: 'mongodb+srv://Guest:82cvM6Ykf7Augf2@f1blogger.itow09w.mongodb.net/?retryWrites=true&w=majority&appName=F1Blogger',
-    NEO4J_SCHEME: 'neo4j+s',
-    NEO4J_HOST: 'f23c0173.databases.neo4j.io',
-    NEO4J_PORT: '7687',
-    NEO4J_USERNAME: 'neo4j',
-    NEO4J_PASSWORD: 'fy5LPDHxXLhyfjn_FO2rPImjGh8X467o1v7iuZovgJU',
-    NEO4J_DATABASE: 'F1BloggerUsers',
 }
