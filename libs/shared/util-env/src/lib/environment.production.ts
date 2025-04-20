16333f0b85d5accf116ed2b082d83e5014d62556
Matthijs07g <matthijs07g@gmail.com>
2025-04-20 22:47:21 +0200

    refactor: update styling and layout for user list, driver list, and about page; enhance user experience with improved card design and responsive adjustments

--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -4,4 +4,10 @@ export const environment: IEnvironment = {
     production: true,
     apiUrl: 'https://f1blogger.azurewebsites.net/api/',
     MONGO_DB_CONNECTION_STRING: 'mongodb+srv://Guest:82cvM6Ykf7Augf2@f1blogger.itow09w.mongodb.net/?retryWrites=true&w=majority&appName=F1Blogger',
+    // NEO4J_SCHEME: 'neo4j+s',
+    // NEO4J_HOST: 'f23c0173.databases.neo4j.io',
+    // NEO4J_PORT: '7687',
+    // NEO4J_USERNAME: 'neo4j',
+    // NEO4J_PASSWORD: 'fy5LPDHxXLhyfjn_FO2rPImjGh8X467o1v7iuZovgJU',
+    // NEO4J_DATABASE: 'F1BloggerUsers',
 }
